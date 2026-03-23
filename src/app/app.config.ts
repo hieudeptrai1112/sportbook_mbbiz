a6ef1ff11beb9e409fdf6abcd0b15db46245d81e
hieudeptrai1112 <a6vuminhhieu@gmail.com>
2026-03-23 22:15:00 +0700

    add ng-zorro and update angular config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideNzI18n(en_US)],
 };
