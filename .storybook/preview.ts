ff548f303d7ef3c90271808dd43d58201c89cde9
hieudeptrai1112 <a6vuminhhieu@gmail.com>
2026-03-23 10:55:27 +0700

    init mbbiz ds

---
+++ .storybook/preview.ts
@@ -0,0 +1,29 @@
+import type { Preview } from '@storybook/angular'
+import { setCompodocJson } from "@storybook/addon-docs/angular";
+import docJson from "../documentation.json";
+setCompodocJson(docJson);
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+    options: {
+      storySort: {
+        order: [
+          'Introduction',
+          'Design Tokens',
+          'Design Guidelines',
+          'Components',
+          'Component Usage',
+          'Development Guide',
+        ],
+      },
+    },
+  },
+};
+
+export default preview;
