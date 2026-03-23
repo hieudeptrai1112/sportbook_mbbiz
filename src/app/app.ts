932a6435e9ca8d230e63754378745803d5edfefa
hieudeptrai1112 <a6vuminhhieu@gmail.com>
2026-03-23 17:05:04 +0700

    add collapsible sidebar groups

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,8 +9,23 @@ import { Component, signal } from '@angular/core';
 export class App {
   protected readonly title = signal('mbbiz-ds');
   protected readonly isDark = signal(false);
+  protected readonly openGroups = signal<Record<string, boolean>>({
+    development: true,
+    components: false,
+    language: false,
+    usage: false,
+    lab: false,
+    pro: false
+  });
 
   toggleTheme(): void {
     this.isDark.update((value) => !value);
+  }
+
+  toggleGroup(key: string): void {
+    this.openGroups.update((state) => ({
+      ...state,
+      [key]: !state[key]
+    }));
   }
 }
